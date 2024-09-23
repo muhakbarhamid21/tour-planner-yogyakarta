0a52330307d71fc872846f4b1859f5e4fc2d5707
Muhammad Akbar Hamid <muhakbarhamid21@gmail.com>
2024-09-23 21:15:24 +0700

    fix: new path

--- apps/authentication/routes.py
+++ apps/authentication/routes.py
@@ -2,10 +2,10 @@ from flask import Blueprint, render_template
 
 accounts_bp = Blueprint('accounts', __name__)
 
-@accounts_bp.route('/accounts/login')
-def login():
-    return render_template('accounts/login.html')
+@accounts_bp.route('/accounts/signin')
+def signin():
+    return render_template('accounts/signin.html')
 
-@accounts_bp.route('/accounts/register')
-def register():
-    return render_template('accounts/register.html')
+@accounts_bp.route('/accounts/signup')
+def signup():
+    return render_template('accounts/signup.html')
