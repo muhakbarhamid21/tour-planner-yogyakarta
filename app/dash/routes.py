44b4342bfa8102b90b3f687dba4cc4561ba62981
Sugeng D. Cahyo <34049790+sugengdcahyo@users.noreply.github.com>
2024-10-09 00:17:37 +0700

    Handle authentication in dashboard route

--- app/dash/routes.py
+++ app/dash/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request
 import jwt, os
+from middleware.auth import is_authenticated
 from .services import AuthService
 
 dash_bp = Blueprint('dash', __name__, root_path="/dashboard")
@@ -25,11 +26,11 @@ Note of menus
 
 
 @dash_bp.route('/dashboard', methods=['GET'])
+@is_authenticated
 def dashboard():
     token = request.cookies.get('token')
 
     data = {}
 
     cookies_data = jwt.decode(token, os.getenv('SECRET_KEY'), algorithms=['HS256'])
-    print(cookies_data)
     return render_template("dash/index.html", data=data)
--- app/home/routes.py
+++ app/home/routes.py
@@ -1,7 +1,22 @@
+import os, jwt
+
 from flask import Blueprint, render_template
+from flask import request
 
 home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/')
 def index():
-    return render_template('home/index.html')  # Ensure this path matches the actual template location
+    token_valid = False
+    token = request.cookies.get('token', None)
+
+    if token:
+        try:
+            jwt.decode(token, os.environ.get("SECRET_KEY", None), algorithms=['HS256'])
+            token_valid = True
+        except jwt.ExpiredSignatureError:
+            pass
+        except jwt.InvalidTokenError:
+            pass
+
+    return render_template('home/index.html', token_valid=token_valid)  # Ensure this path matches the actual template location
