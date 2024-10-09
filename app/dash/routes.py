75c47314ef54bdfdce00a3571edd2f06025ae2eb
Muhammad Akbar Hamid <muhakbarhamid21@gmail.com>
2024-10-09 14:40:18 +0700

    fix: count type of attraction

--- app/dash/routes.py
+++ app/dash/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request
 import jwt, os
-from app.dash.services2 import get_attraction_counts
+from app.dash.services2 import get_attraction_count_for_category
 from middleware.auth import is_authenticated
 from .services import AuthService
 
@@ -31,10 +31,16 @@ from flask import g
 def dashboard():
     token = request.cookies.get('token')
     
-    category_counts = get_attraction_counts()
+    count_category_1 = get_attraction_count_for_category(1)
+    count_category_2 = get_attraction_count_for_category(2)
+    count_category_3 = get_attraction_count_for_category(3)
+    count_category_4 = get_attraction_count_for_category(4)
     
     data = {
-        "category_counts": category_counts
+        "count_category_1": count_category_1,
+        "count_category_2": count_category_2,
+        "count_category_3": count_category_3,
+        "count_category_4": count_category_4,
     }
     
     cookies_data = jwt.decode(token, os.getenv('SECRET_KEY'), algorithms=['HS256'])
